cde6938c215025b7ae22d9caa276e299e7e33917
angelo-lepore <angelo.lepore94@gmail.com>
2025-05-29 16:09:47 +0200

    started-02-arrays-bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,17 +10,32 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+
+const reversedTeachers = teachers.reverse ();
+console.log (teachers); // ["Luca", "Lewis", "Carlo", "Phil", "Fabio", "Ed", "Nathan"]
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  const teacherName = teachers[i];
+  if (teacherName.length >= 5) {
+    longNames.push (teacherName);
+  }
+}
+console.log (longNames); // ["Lewis", "Carlo", "Fabio", "Nathan"]
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+teachers.splice (5, 1);
+console.log (teachers); // ["Luca", "Lewis", "Carlo", "Phil", "Fabio", "Nathan"]
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+
 const isFabioPresent = null;
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+
 const teachersString = null;
